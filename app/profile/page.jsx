e8baa4bb44766a7d2326330c2303cf417bfa9798
Mohammad <mehmandoost2@gmail.com>
2023-08-15 02:02:22 +0430

    newpage

---
+++ app/profile/page.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const metadata = {
+  title: "profile",
+  description: "new profile",
+};
+
+const profile = () => {
+  return <div>hi</div>;
+};
+
+export default profile;
