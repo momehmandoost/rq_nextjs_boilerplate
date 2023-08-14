faa17092d5e58d16ff2983321d5eee7ab1fc4675
Mohammad <mehmandoost2@gmail.com>
2023-08-15 01:41:28 +0430

    first commit

---
+++ app/components/Reactqy.jsx
@@ -0,0 +1,21 @@
+"use client";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 0,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
+export default function Reactqy({ children }) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+}
